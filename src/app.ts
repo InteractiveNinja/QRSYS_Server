43d6fe34ebbd30ee35207e242f4dcb409a045d26
InteractiveNinja <gabrielnadolny2@gmail.com>
2021-03-21 17:36:28 +0100

    error catching for json

--- src/app.ts
+++ src/app.ts
@@ -10,22 +10,28 @@ const server = new Server({ port })
 
 server.on('connection', (socket, req) => {
     socket.on('message', socketData => {
-        let json = JSON.parse(socketData.toString())
-        switch (json.type) {
-            case "register":
-                registerSocket(json.value, socket, req.socket.remoteAddress).then(e => socket.send(JSON.stringify({ "type": "callback", "value": "200" }))).catch(e => socket.send(JSON.stringify({ "type": "callback", "value": "500" })))
-                break;
-            case "send":
-                sendToSocket(json.userid,json.deviceid,json.message).then(e => socket.send("200")).catch(e => socket.send("500"))
-                break;
-            case "list":
-                listSockets(json.value).then(e => socket.send(JSON.stringify(e))).catch(e => socket.send("500"))
-                break;
-            default:
-                socket.send(JSON.stringify({ "type": "callback", "value": "500" }))
-                socket.close()
-                break;
+        try {
+            let json = JSON.parse(socketData.toString())
+            switch (json.type) {
+                case "register":
+                    registerSocket(json.value, socket, req.socket.remoteAddress).then(e => socket.send(JSON.stringify({ "type": "callback", "value": "200" }))).catch(e => socket.send(JSON.stringify({ "type": "callback", "value": "500" })))
+                    break;
+                case "send":
+                    sendToSocket(json.userid,json.deviceid,json.message).then(e => socket.send(JSON.stringify({ "type": "callback", "value": "200" }))).catch(e => socket.send(JSON.stringify({ "type": "callback", "value": "500" })))
+                    break;
+                case "list":
+                    listSockets(json.value).then(e => socket.send(JSON.stringify(e))).catch(e => socket.send(JSON.stringify({ "type": "callback", "value": "500" })))
+                    break;
+                default:
+                    socket.send(JSON.stringify({ "type": "callback", "value": "500" }))
+                    socket.close()
+                    break;
+            }
+        } catch (error) {
+            socket.send(JSON.stringify({ "type": "callback", "value": "500" }))
+            socket.close()
         }
+       
     })
 
 })
