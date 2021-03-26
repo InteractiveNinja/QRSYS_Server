b6f87aefef352cbb626ca2586ecbdd07a24470ba
InteractiveNinja <gabrielnadolny2@gmail.com>
2021-03-26 07:16:18 +0100

    added dotenv

--- src/app.ts
+++ src/app.ts
@@ -2,9 +2,15 @@ import { DB } from './Database';
 import { AuthServer } from './authServer';
 import { config as c } from '@interactiveninja/config-reader'
 import {SocketServer} from './socketServer'
-
+import {config as env} from 'dotenv'
 const config = c("config.json")
 
+//Ladet Umgebungsvariablen
+env()
+
+console.log(process.env.DB)
+
+// Ladet die verschieden Module
 let database = new DB(config)
 let socket = new SocketServer(config,database)
 let auth = new AuthServer(config,database)
